bb6dfb4367a53fbdd1a73b1115d290875e6da07d
Chris Whong <chris.m.whong@gmail.com>
2019-09-01 23:38:34 -0400

    refactor scraper, modify dir structure

--- scripts/README.md
+++ scripts/README.md
@@ -4,6 +4,10 @@ Use pdftotext on the command line to turn the capital commitment plan pdf into t
 
 5.3 seems to be the magic number to get the output txt to have predictable and uniform columns on each lines
 
-run  `node scrape path-to-pdftotext-output.txt`
+run  `node scrape txtdirectory`
 
 There seems to be an anomaly on the last page of the pdf where the 3-digit number next to a commit `001` is only two digits.  The leading zero was filled in manually to make sure this data was included in the output.
+
+
+Import to DB
+`mongoimport --uri {MONGO_URI} --jsonArray --collection ccp-10-18a --file ../data/ccp-10-18a.json`
--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,8 +1,7 @@
 const fs = require('fs-extra')
 const readline = require('readline')
 
-// fs.ensureFileSync(outputPath);
-// const output = fs.createWriteStream(outputPath);
+const inputDir = process.argv[2]
 
 const formatCost = (rawCost) => {
   let negative = false
@@ -27,191 +26,145 @@ const parseBudgetLineHeaders = (line) => {
   return { city, nonCity }
 }
 
-let budgetLineObject = null
-let capitalProjects = []
-let capitalProjectObject = {}
-let commitmentDetails = []
+const parseLine = (line, budgetLine) => {
+  // const closeCapitalProject = () => {
+  //   // add the array of commitmentdetails to the current project
+  //   capitalProjectObject.commitmentDetails = commitmentDetails
+  //   commitmentDetails = [] // reset
+  //
+  //   // if the capital project exists, push it to the capitalProjects array
+  //   capitalProjectObject.id && capitalProjects.push(capitalProjectObject)
+  //   capitalProjectObject = {} // reset
+  // }
 
-// { // budgetLineObject
-//   ...,
-//   projects: [ // capitalProjects
-//     ...,
-//     commitmentDetails: [
-//       ...
-//     ]
-//   ]
-// }
-
-const closeCapitalProject = () => {
-  // add the array of commitmentdetails to the current project
-  capitalProjectObject.commitmentDetails = commitmentDetails
-  commitmentDetails = [] // reset
-
-  // if the capital project exists, push it to the capitalProjects array
-  capitalProjectObject.id && capitalProjects.push(capitalProjectObject)
-  capitalProjectObject = {} // reset
-}
-
-// wrap up all running values and output a complete budgetLineItem
-const closeBudgetLineObject = () => {
-  // close up the last project
-  closeCapitalProject()
-
-  budgetLineObject.projects = capitalProjects
-  console.log('Complete Budget Line', JSON.stringify(budgetLineObject, null, 2))
-
-  // reset all the things
-  budgetLineObject = null
-  capitalProjects = []
-  commitmentDetails = []
-}
-
-const parseLine = (line) => {
+  // // wrap up all running values and output a complete budgetLineItem
+  // const closeBudgetLineObject = () => {
+  //   // close up the last project
+  //   closeCapitalProject()
+  //
+  //   budgetLineObject.projects = capitalProjects
+  //   // console.log('Complete Budget Line', JSON.stringify(budgetLineObject, null, 2))
+  //   output.write(`${i > 0 ? ',' : ''}${JSON.stringify(budgetLineObject, null, 2)}`)
+  //
+  //   i += 1
+  //   // reset all the things
+  //   budgetLineObject = null
+  //   capitalProjects = []
+  //   commitmentDetails = []
+  // }
   // check for patterns of new budget line, new project, or commitment
   if (/BUDGET LINE/.test(line)) { // new budget line
-    budgetLineObject && closeBudgetLineObject()
-
-    budgetLineObject = {}
-
-    budgetLineObject.budgetLine = line.match(/BUDGET LINE:(.*)FMS/)[1].trim()
-    budgetLineObject.fmsNumber = line.match(/FMS #:(.{1,10}).*/)[1].trim()
-    budgetLineObject.description = line.match(/FMS #:.{1,10}(.*)/)[1].trim()
-
-    return
-  }
-
-  if (/TOTALS FOR/.test(line)) {
-    console.log('HERE', commitmentDetails)
-    // if we hit a totals for line, close out the current budgetLineObject
-    budgetLineObject && closeBudgetLineObject()
-  }
-
-  if (budgetLineObject) { // don't match any other lines unless there's a budgetLineObject
-    if (/AVAILABLE BALANCE AS OF/.test(line)) {
-      if (budgetLineObject.budgetLine && !budgetLineObject.availableBalance) { // only log values if following a budget line heading (exclude totals)
-        const asOf = line.match(/\d{2}\/\d{2}\/\d{2}/)[0]
-        const { city, nonCity } = parseBudgetLineHeaders(line)
-        budgetLineObject.availableBalance = {
-          asOf,
-          city,
-          nonCity,
-          total: city + nonCity
-        }
+    budgetLine.budgetLineId = line.match(/BUDGET LINE:(.*)FMS/)[1].trim()
+    budgetLine.fmsNumber = line.match(/FMS #:(.{1,10}).*/)[1].trim()
+    budgetLine.description = line.match(/FMS #:.{1,10}(.*)/)[1].trim()
+    console.log('New Budget Line', budgetLine.description)
+  } else if (/AVAILABLE BALANCE AS OF/.test(line)) {
+    if (budgetLine.budgetLineId && !budgetLine.availableBalance) { // only log values if following a budget line heading (exclude totals)
+      const asOf = line.match(/\d{2}\/\d{2}\/\d{2}/)[0]
+      const { city, nonCity } = parseBudgetLineHeaders(line)
+      budgetLine.availableBalance = {
+        asOf,
+        city,
+        nonCity,
+        total: city + nonCity
       }
-
-      return
     }
+  } else if (/CONTRACT LIABILITY/.test(line)) {
+    if (budgetLine.budgetLineId && !budgetLine.contractLiability) { // only log values if following a budget line heading (exclude totals)
+      const { city, nonCity } = parseBudgetLineHeaders(line)
 
-    if (/CONTRACT LIABILITY/.test(line)) {
-      if (budgetLineObject.budgetLine && !budgetLineObject.contractLiability) { // only log values if following a budget line heading (exclude totals)
-        const { city, nonCity } = parseBudgetLineHeaders(line)
-
-        budgetLineObject.contractLiability = {
-          city,
-          nonCity
-        }
+      budgetLine.contractLiability = {
+        city,
+        nonCity
       }
-
-      return
     }
+  } else if (/ITD EXPENDITURES/.test(line)) {
+    if (budgetLine.budgetLineId && !budgetLine.itdExpenditures) { // only log values if following a budget line heading (exclude totals)
+      const { city, nonCity } = parseBudgetLineHeaders(line)
 
-    if (/ITD EXPENDITURES/.test(line)) {
-      if (budgetLineObject.budgetLine && !budgetLineObject.itdExpenditures) { // only log values if following a budget line heading (exclude totals)
-        const { city, nonCity } = parseBudgetLineHeaders(line)
-
-        budgetLineObject.itdExpenditures = {
-          city,
-          nonCity
-        }
+      budgetLine.itdExpenditures = {
+        city,
+        nonCity
       }
-
-      return
     }
-
-    if (/ADOPTED/.test(line)) {
-      if (budgetLineObject.budgetLine && !budgetLineObject.adoptedAppropriations) { // only log values if following a budget line heading (exclude totals)
-        const [,
-          adoptedFY19City,
-          adoptedFY20City,
-          adoptedFY21City,
-          adoptedFY22City,
-          ,
-          commitmentFY19City,
-          commitmentFY20City,
-          commitmentFY21City,
-          commitmentFY22City
-        ] = line.split('*')
-
-        budgetLineObject.adoptedAppropriations = {
-          fy19: {
-            city: formatCost(adoptedFY19City)
-          },
-          fy20: {
-            city: formatCost(adoptedFY20City)
-          },
-          fy21: {
-            city: formatCost(adoptedFY21City)
-          },
-          fy22: {
-            city: formatCost(adoptedFY22City)
-          }
-        }
-
-        budgetLineObject.commitmentPlan = {
-          fy19: {
-            city: formatCost(commitmentFY19City)
-          },
-          fy20: {
-            city: formatCost(commitmentFY20City)
-          },
-          fy21: {
-            city: formatCost(commitmentFY21City)
-          },
-          fy22: {
-            city: formatCost(commitmentFY22City)
-          }
-        }
-      }
-
-      return
-    }
-
-    if (/^\s*\(N\)*/.test(line)) {
+  } else if (/ADOPTED/.test(line)) {
+    if (budgetLine.budgetLineId && !budgetLine.adoptedAppropriations) { // only log values if following a budget line heading (exclude totals)
       const [,
-        adoptedFY19NonCity,
-        adoptedFY20NonCity,
-        adoptedFY21NonCity,
-        adoptedFY22NonCity,
+        adoptedFY19City,
+        adoptedFY20City,
+        adoptedFY21City,
+        adoptedFY22City,
         ,
-        commitmentFY19NonCity,
-        commitmentFY20NonCity,
-        commitmentFY21NonCity,
-        commitmentFY22NonCity
+        commitmentFY19City,
+        commitmentFY20City,
+        commitmentFY21City,
+        commitmentFY22City
       ] = line.split('*')
 
-      const { adoptedAppropriations, commitmentPlan } = budgetLineObject
-      adoptedAppropriations.fy19.nonCity = formatCost(adoptedFY19NonCity)
-      adoptedAppropriations.fy20.nonCity = formatCost(adoptedFY20NonCity)
-      adoptedAppropriations.fy21.nonCity = formatCost(adoptedFY21NonCity)
-      adoptedAppropriations.fy22.nonCity = formatCost(adoptedFY22NonCity)
+      budgetLine.adoptedAppropriations = {
+        fy19: {
+          city: formatCost(adoptedFY19City)
+        },
+        fy20: {
+          city: formatCost(adoptedFY20City)
+        },
+        fy21: {
+          city: formatCost(adoptedFY21City)
+        },
+        fy22: {
+          city: formatCost(adoptedFY22City)
+        }
+      }
 
-      commitmentPlan.fy19.nonCity = formatCost(commitmentFY19NonCity)
-      commitmentPlan.fy20.nonCity = formatCost(commitmentFY20NonCity)
-      commitmentPlan.fy21.nonCity = formatCost(commitmentFY21NonCity)
-      commitmentPlan.fy22.nonCity = formatCost(commitmentFY22NonCity)
-
-      return
+      budgetLine.commitmentPlan = {
+        fy19: {
+          city: formatCost(commitmentFY19City)
+        },
+        fy20: {
+          city: formatCost(commitmentFY20City)
+        },
+        fy21: {
+          city: formatCost(commitmentFY21City)
+        },
+        fy22: {
+          city: formatCost(commitmentFY22City)
+        }
+      }
     }
+  } else if (/^\s*\(N\)*/.test(line)) {
+    const [,
+      adoptedFY19NonCity,
+      adoptedFY20NonCity,
+      adoptedFY21NonCity,
+      adoptedFY22NonCity,
+      ,
+      commitmentFY19NonCity,
+      commitmentFY20NonCity,
+      commitmentFY21NonCity,
+      commitmentFY22NonCity
+    ] = line.split('*')
 
-    if (/^\s*\d{3}\s/.test(line)) { // beginning of new capital project
-      closeCapitalProject()
+    const { adoptedAppropriations, commitmentPlan } = budgetLine
+    adoptedAppropriations.fy19.nonCity = formatCost(adoptedFY19NonCity)
+    adoptedAppropriations.fy20.nonCity = formatCost(adoptedFY20NonCity)
+    adoptedAppropriations.fy21.nonCity = formatCost(adoptedFY21NonCity)
+    adoptedAppropriations.fy22.nonCity = formatCost(adoptedFY22NonCity)
 
-      capitalProjectObject.managingAgency = line.match(/\d{3}/)[0]
-      capitalProjectObject.id = line.match(/\d{3}(.{1,10})/)[1].trim()
-      capitalProjectObject.description = `${line.match(/\d{3}.{1,10}(.{1,62})/)[1].replace(/"/g, '').replace(/ +(?= )/g, '').trim()}`
+    commitmentPlan.fy19.nonCity = formatCost(commitmentFY19NonCity)
+    commitmentPlan.fy20.nonCity = formatCost(commitmentFY20NonCity)
+    commitmentPlan.fy21.nonCity = formatCost(commitmentFY21NonCity)
+    commitmentPlan.fy22.nonCity = formatCost(commitmentFY22NonCity)
+  } else if (/^\s*\d{3}\s/.test(line)) { // beginning of new capital project
+    if (budgetLine.budgetLineId) {
+      const project = budgetLine.projects[budgetLine.projects.length - 1]
+      project.managingAgency = line.match(/\d{3}/)[0]
+      project.id = line.match(/\d{3}(.{1,10})/)[1].trim()
+      project.description = `${line.match(/\d{3}.{1,10}(.{1,62})/)[1].replace(/"/g, '').replace(/ +(?= )/g, '').trim()}`
+      console.log('    Capital Project', project.description)
     }
-    console.log(line)
-    if (/\s[A-Z]{4}\s.{1,5}\s\d{3}\s/.test(line)) { // commitments
+  } else if (/\s[A-Z]{4}\s.{2}\s\S{1,3}\s/.test(line)) { // commitments
+    if (budgetLine.budgetLineId) {
+      const project = budgetLine.projects[budgetLine.projects.length - 1]
       const category = line.substring(18, 22)
       const subcategory = line.substring(23, 25).trim()
       const code = line.substring(26, 29)
@@ -237,19 +190,62 @@ const parseLine = (line) => {
         planCommDate
       }
 
-      commitmentDetails.push(commitDetailObject)
+      project.commitments.push(commitDetailObject)
+      console.log(line)
+      console.log('        Commitment', categoryDescription)
     }
   }
 
-  return null
+  return budgetLine
 }
 
-// run this script like >node scrape path to txt file
-const readInterface = readline.createInterface({
-  input: fs.createReadStream(process.argv[2]),
-  console: false
-})
+const parseTxtFile = (inputDir, file) => {
+  console.log(inputDir, file)
+  // create an output file, add open array bracket
+  const outputPath = `../data/ccp-10-18/json/${file.split('.')[0]}.json`
+  fs.ensureFileSync(outputPath)
+  const output = fs.createWriteStream(outputPath)
+  output.write('[\n')
 
-readInterface.on('line', (line) => {
-  parseLine(line)
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(`${inputDir}/${file}`),
+    console: false
+  })
+
+  let i = 0
+  let budgetLine = {
+    projects: []
+  }
+
+  readInterface.on('line', (line) => {
+    // check to see if current line closes previous budgetLine
+    // if so, write it to file and reset before proceeding
+    const closeBudgetLine = !!/TOTALS FOR/.test(line) || !!/BUDGET LINE/.test(line)
+    if (closeBudgetLine && budgetLine.budgetLineId) {
+      output.write(`${i > 0 ? ',' : ''}${JSON.stringify(budgetLine, null, 2)}`)
+      i += 1
+      budgetLine = {
+        projects: []
+      }
+    }
+
+    // if the line should start a new capital project, insert an empty object into budgetline.projects
+    if (budgetLine.budgetLineId && /^\s*\d{3}\s/.test(line)) {
+      budgetLine.projects.push({
+        commitments: []
+      })
+    }
+
+    budgetLine = parseLine(line, budgetLine)
+  })
+
+  readInterface.on('close', () => {
+    output.write(']')
+  })
+}
+
+const files = fs.readdirSync(`${inputDir}`)
+files.forEach((file) => {
+  console.log(`Parsing ${inputDir}/${file}`)
+  parseTxtFile(inputDir, file)
 })
